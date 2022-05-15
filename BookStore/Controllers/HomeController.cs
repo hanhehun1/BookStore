1b1a3dbd8da94a1fcc36d30836697710fff51aa9
hanhehun <khanhtqgcd18464@fpt.edu.vn>
2022-05-16 04:17:11 +0700

    Add

--- BookStore/Controllers/HomeController.cs
+++ BookStore/Controllers/HomeController.cs
@@ -1,30 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using BookStore.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Data.Entity;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace BookStore.Controllers
 {
-    public class HomeController : Controller
-    {
-        public ActionResult Index()
-        {
-            return View();
-        }
+	public class HomeController : Controller
+	{
+		private ApplicationUser _user;
+		private ApplicationDbContext _context;
+		private UserManager<ApplicationUser> _usermanager;
+		public HomeController()
+		{
+			_user = new ApplicationUser();
+			_context = new ApplicationDbContext();
+			_usermanager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
+		}
+		public ActionResult Index(string searchString)
+		{
+			var book = _context.Book.Include(t => t.Category).ToList();
+			if (!String.IsNullOrWhiteSpace(searchString))
+			{
+				book = _context.Book
+				.Where(t => t.Title.Contains(searchString))
+				.Include(t => t.Category)
+				.ToList();
+			}
+			return View(book);
+		}
+		public ActionResult DetailBook(int id)
+		{
+			var detailBook = _context.Book.Include(t => t.Category).SingleOrDefault(t => t.Id == id);
+			return View(detailBook);
+		}
+		public ActionResult About()
+		{
+			ViewBag.Message = "Your application description page.";
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
+			return View();
+		}
+		public ActionResult Help()
+		{
 
-            return View();
-        }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+			return View();
+		}
 
-            return View();
-        }
-    }
+		public ActionResult Contact()
+		{
+			ViewBag.Message = "Your contact page.";
+
+			return View();
+		}
+	}
 }
