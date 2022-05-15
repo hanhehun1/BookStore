27f6e3b21c3492ec17fa9b4646ede4aa72d1f4e9
hanhehun <khanhtqgcd18464@fpt.edu.vn>
2022-05-15 16:10:54 +0700

    View model

---
+++ BookStore/ViewModel/CategoryBookViewModel.cs
@@ -0,0 +1,12 @@
+﻿using BookStore.Models;
+using System.Collections.Generic;
+
+namespace BookStore.ViewModel
+{
+	public class CategoryBookViewModel
+	{
+		public Book Book { get; set; }
+		public int Id { get; set; }
+		public IEnumerable<Category> Category { get; set; }
+	}
+}
---
+++ BookStore/ViewModel/OrderDetailViewModel.cs
@@ -0,0 +1,23 @@
+﻿using BookStore.Models;
+using System;
+using System.Collections.Generic;
+
+namespace BookStore.ViewModel
+{
+	public class OrderDetailViewModel
+	{
+		public string BookId { get; set; }
+		public string UserId { get; set; }
+		public Book Book { get; set; }
+		public float Quantity { get; set; }
+		public float TotalPrice { get; set; }
+		public DateTime DateTime { get; set; }
+		public OrderDetailViewModel()
+		{
+			DateTime = DateTime.Now;
+		}
+		public List<OrderDetail> OrderDetail { get; set; }
+
+		public IEnumerable<User> User { get; set; }
+	}
+}
